8910a02c4e909c68ecb3b4b85d8350f40ccf5275
Roger Ruiz <roger.ruizb@ug.edu.ec>
2024-02-24 15:01:43 -0500

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorilaScreen extends StatelessWidget {
+  static const name = 'tutorial_screen';
+
+  const AppTutorilaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

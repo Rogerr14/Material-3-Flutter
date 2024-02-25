88b0a73351a24f267486846481ebb7371f003a6f
Roger Ruiz <roger.ruizb@ug.edu.ec>
2024-02-25 01:12:14 -0500

    ui screen terminado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -36,4 +36,9 @@ final appRouter = GoRouter(
     name: AnimatedScreen.name,
     builder: (context, state) => const AnimatedScreen(),
   ),
+  GoRoute(
+    path: '/ui-controls',
+    name: UiControlsScreen.name,
+    builder: (context, state) => const UiControlsScreen(),
+  ),
 ]);
